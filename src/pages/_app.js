727046a82b2a50f9dd968c86de4571d3fb727437
Ujjwal Giri <ug.online.st@gmail.com>
2023-10-11 23:48:42 +0545

    Fixed Issues

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
 import { Montserrat } from "next/font/google";
 import Head from "next/head";
+import img from "../../public/images/profile3.png";
 
 // initializing the font with variable
 const montserrat = Montserrat({
@@ -15,7 +16,8 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <link rel="icon" href={img} />
       </Head>
       <main
         className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen`}
